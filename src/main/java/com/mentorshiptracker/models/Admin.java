c9ef0caa01ddd9808face542a9baad18ed047b27
winfred.agbotey <winfred.mawuli-agbotey@amalitech.com>
2023-06-26 15:21:41 +0000

    feat: add entities

--- src/main/java/com/mentorshiptracker/models/Admin.java
+++ src/main/java/com/mentorshiptracker/models/Admin.java
@@ -1,2 +1,29 @@
-package com.mentorshiptracker.models;public class Admin {
+package com.mentorshiptracker.models;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "admins")
+public class Admin {
+    @Id
+    private UUID id;
+    private String username;
+    private String email;
+    private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
 }
--- src/main/java/com/mentorshiptracker/models/Role.java
+++ src/main/java/com/mentorshiptracker/models/Role.java
@@ -1,2 +1,29 @@
-package com.mentorshiptracker.models;public class Role {
+package com.mentorshiptracker.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String name;
+    private String description;
+
+    @ManyToMany
+    @JoinTable(name = "role_permissions",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "permission_id")
+    )
+    private List<Permission> permissions;
 }
