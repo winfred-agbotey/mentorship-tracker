1d1bda4ca16c136128b7a6ef4cd10059e2f9cf44
winfred.agbotey <winfred.mawuli-agbotey@amalitech.com>
2023-06-26 15:23:06 +0000

    add migrations

---
+++ src/main/resources/db/migration/V1__init_database.sql
@@ -0,0 +1,33 @@
+-- Create roles table
+CREATE TABLE roles
+(
+    id          UUID PRIMARY KEY,
+    name        VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL
+);
+-- Create admins table
+CREATE TABLE admins
+(
+    id       UUID PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email    VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role_id  UUID REFERENCES roles (id)
+);
+
+-- Create permissions table
+CREATE TABLE permissions
+(
+    id          UUID PRIMARY KEY,
+    name        VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL
+);
+
+
+-- Create role_permissions table for the many-to-many relationship
+CREATE TABLE role_permissions
+(
+    role_id       UUID REFERENCES roles (id),
+    permission_id UUID REFERENCES permissions (id),
+    PRIMARY KEY (role_id, permission_id)
+);
