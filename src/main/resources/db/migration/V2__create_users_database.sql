d45de7738132f2f3d3c1eb540e84ed9006500e44
winfred.agbotey <winfred.mawuli-agbotey@amalitech.com>
2023-07-05 15:16:43 +0000

    pref: migration updates

---
+++ src/main/resources/db/migration/V2__create_users_database.sql
@@ -0,0 +1,31 @@
+-- Create locations table
+CREATE TABLE locations
+(
+    id            UUID PRIMARY KEY,
+    country       VARCHAR(255) NOT NULL,
+    city          VARCHAR(255) NOT NULL,
+    date_created  TIMESTAMP,
+    date_modified TIMESTAMP
+);
+
+-- Create advisors table
+CREATE TABLE advisors
+(
+    user_id       UUID PRIMARY KEY REFERENCES users (id),
+    role_id       UUID REFERENCES roles (id),
+    date_of_birth DATE,
+    location_id   UUID REFERENCES locations (id),
+    CONSTRAINT fk_advisors_user_id FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+-- Create advisees table
+CREATE TABLE advisees
+(
+    user_id       UUID PRIMARY KEY REFERENCES users (id),
+    role_id       UUID REFERENCES roles (id),
+    date_of_birth DATE,
+    location_id   UUID REFERENCES locations (id),
+    advisor_id    UUID REFERENCES advisors (user_id),
+    CONSTRAINT fk_advisees_user_id FOREIGN KEY (user_id) REFERENCES users (id),
+    CONSTRAINT fk_advisees_advisor_id FOREIGN KEY (advisor_id) REFERENCES advisors (user_id)
+);
