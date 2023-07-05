f93ae359b1e3f9f1aaaa775bbb2ef1481fdbd5f2
winfred.agbotey <winfred.mawuli-agbotey@amalitech.com>
2023-07-05 15:14:06 +0000

    pref: add custom exception handler

--- src/main/java/com/mentorshiptracker/exceptions/ErrorResponse.java
+++ src/main/java/com/mentorshiptracker/exceptions/ErrorResponse.java
@@ -1,2 +1,15 @@
-package com.mentorshiptracker.exceptions;public class ErrorResponse {
+package com.mentorshiptracker.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Getter
+public class ErrorResponse {
+    private LocalDateTime timestamp;
+    private String message;
+    private String description;
+
 }
--- src/main/java/com/mentorshiptracker/exceptions/UserException.java
+++ src/main/java/com/mentorshiptracker/exceptions/UserException.java
@@ -1,2 +1,14 @@
-package com.mentorshiptracker.exceptions;public class UserException {
+package com.mentorshiptracker.exceptions;
+
+
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class UserException extends RuntimeException{
+    public UserException(String message){
+        super(message);
+    }
 }
