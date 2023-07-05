1fb0acde1c18fb4c83e4f6397bcf06324d578aa4
winfred.agbotey <winfred.mawuli-agbotey@amalitech.com>
2023-07-05 15:17:41 +0000

    pref: add entity dtos

--- src/main/java/com/mentorshiptracker/dtos/AdminRequestDTO.java
+++ src/main/java/com/mentorshiptracker/dtos/AdminRequestDTO.java
@@ -1,2 +1,14 @@
-package com.mentorshiptracker.dtos;public class AdminRequestDTO {
+package com.mentorshiptracker.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdminRequestDTO {
+    private String username;
+    private String email;
+    private String password;
 }
--- src/main/java/com/mentorshiptracker/dtos/AdminResponseDTO.java
+++ src/main/java/com/mentorshiptracker/dtos/AdminResponseDTO.java
@@ -1,2 +1,20 @@
-package com.mentorshiptracker.dtos;public class AdminResponseDTO {
+package com.mentorshiptracker.dtos;
+
+import com.mentorshiptracker.models.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdminResponseDTO {
+    private String username;
+    private String email;
+    private Role role;
+    private LocalDateTime dateCreated;
+    private LocalDateTime dateModified;
+
 }
